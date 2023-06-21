52dcf45e20656525540d3e5478154068d42393bc
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-06-21 13:50:20 +0200

    Currency array added

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.290,
+    },
+    {
+        short: "EUR",
+        name:"Euro",
+        rate: 4.640,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.230,
+    },
+];
