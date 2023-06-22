8fd44ac6d08ecc640800ead895b0a28c994993bc
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-06-22 13:06:20 +0200

    calculateResult function added

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import './App.css';
+import { currencies } from './currencies';
 
 function App() {
-  
+    const [result, setResult] = useState();
+
+    const calculateResult = (currency, amount) => {
+        const rate = currencies
+            .find(({ short }) => short === currency)
+            .rate;
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: amount / rate,
+            currency,
+        });
+    };
 }
 
 export default App;
