62e34e4c731997027e2a9692936a7a1cd61ea002
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-09-13 14:24:41 +0200

    added a new Clock component and created a new Date object with the current date and time

---
+++ src/App/Clock/index.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+export const Clock = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <div>
+            Dzisiaj jest
+            {" "}
+            {new Date().toLocaleDateString(undefined, {
+                weekday: "long",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                day: "numeric",
+                month: "long",
+            })};
+        </div>
+    );
+}
